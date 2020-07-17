6f2136f4be8695780ec11599ff14cfb82803a9b9
kgoyal000 <kgoyal00000@gmail.com>
2020-07-17 17:02:01 +0530

    made pwa

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
-    })
-  }
   
 var painting = false;
 var erasing = false;
