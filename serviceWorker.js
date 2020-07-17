724a8a3586bba4a8f41ae34c7370116c0c9714db
kgoyal000 <kgoyal00000@gmail.com>
2020-07-18 01:43:04 +0530

    Again notification 2

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,28 +1,28 @@
-const staticDevCoffee = "paint-app-v1"
-const assets = [
-  "/",
-  "./paint.html",
-  "./css/styles.css",
-  "./js/index.js",
-  "./images/background.png"
-]
+// const staticDevCoffee = "paint-app-v1"
+// const assets = [
+//   "/",
+//   "./paint.html",
+//   "./css/styles.css",
+//   "./js/index.js",
+//   "./images/background.png"
+// ]
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
+// self.addEventListener("install", installEvent => {
+//   installEvent.waitUntil(
+//     caches.open(staticDevCoffee).then(cache => {
+//       cache.addAll(assets)
+//     })
+//   )
+// })
 
 
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
-      })
-    )
-  })
+// self.addEventListener("fetch", fetchEvent => {
+//     fetchEvent.respondWith(
+//       caches.match(fetchEvent.request).then(res => {
+//         return res || fetch(fetchEvent.request)
+//       })
+//     )
+//   })
 
 
 
