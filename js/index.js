9cd42774800059356a43e2eb6f5885c2cfdd8a12
kgoyal000 <kgoyal00000@gmail.com>
2020-07-17 21:40:26 +0530

    added new features and one canvas

--- js/index.js
+++ js/index.js
@@ -1,21 +1,22 @@
 var painting = false;
 var erasing = false;
 var canvas = document.getElementById('paint');
-var canvasMob = document.getElementById('paint1');
 canvas.height = document.querySelector('.canvas').clientHeight;
 canvas.width = document.querySelector('.canvas').clientWidth;
-canvasMob.height = document.querySelector('.canvas').clientHeight;
-canvasMob.width = document.querySelector('.canvas').clientWidth;
+
 
 var ctx = canvas.getContext('2d');
-var ctx1 = canvasMob.getContext('2d');
 ctx.lineWidth = $('#slider').val();
 ctx.lineCap = "round";
 ctx.lineJoin = "round";
-ctx1.lineWidth = $('#slider').val();
-ctx1.lineCap = "round";
-ctx1.lineJoin = "round";
 var slideValue;
+if(localStorage.getItem("imgCanvas") != null){
+    var img = new Image();
+    img.onload = function(){
+    ctx.drawImage(img, 0, 0);
+    }
+    img.src = localStorage.getItem("imgCanvas");
+    };
 function slider(){
     slideValue = $('#slider').val()
     $('.cursor').css('width',slideValue);
@@ -63,32 +64,53 @@ function mouseup(event){
 }
 function touchdown(event){
     painting= true;
-    ctx1.beginPath();
-    ctx1.lineWidth = slideValue;
+    ctx.beginPath();
+    ctx.lineWidth = slideValue;
     var touch = event.touches[0];
     // or taking offset into consideration
-    var x = touch.pageX - canvasMob.offsetLeft;
-    var y = touch.pageY - canvasMob.offsetTop;
-    ctx1.moveTo(x,y)
+    var x = touch.pageX - canvas.offsetLeft;
+    var y = touch.pageY - canvas.offsetTop;
+    ctx.moveTo(x,y)
     console.log(x+" "+y)
 }
 
 function touchmove(event){
     if(painting){
-        ctx1.strokeStyle = $('#color').val();
+        ctx.strokeStyle = $('#color').val();
         var touch = event.touches[0];
         // or taking offset into consideration
-        var x = touch.pageX - canvasMob.offsetLeft;
-        var y = touch.pageY - canvasMob.offsetTop;
+        var x = touch.pageX - canvas.offsetLeft;
+        var y = touch.pageY - canvas.offsetTop;
         if(erasing){
             ctx.strokeStyle = '#ffffff';
-            ctx1.strokeStyle = '#ffffff';
         }
-        ctx1.lineTo(x,y)        
+        ctx.lineTo(x,y)        
     }
-    ctx1.stroke()
+    ctx.stroke()
 }
 
 function touchup(event){
     painting= false;
 }
+
+$("#save").click(function(){
+    if(typeof(localStorage) != null){
+    localStorage.setItem("imgCanvas",
+   canvas.toDataURL());
+    }else{
+    window.alert("Your browser does not support slocal storage!");
+    }
+    });
+
+    $("#reset").click(function(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        $(".erase").removeClass("btn-danger");
+    });
+
+    function download() {
+        var download = document.getElementById("download");
+        var image = document.getElementById("paint").toDataURL("image/png")
+            .replace("image/png", "image/octet-stream");
+        download.setAttribute("href", image);
+        //download.setAttribute("download","archive.png");
+        }
